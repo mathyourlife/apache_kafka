93fa1477ac1b0f23e787be1c746bc1137daea383
Dan Couture <MathYourLife@gmail.com>
2015-02-26 11:45:03 -0500

    Match version in metadata to tag

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email "mathyourlife@gmail.com"
 license          "Apache-2.0"
 description      "Installs/Configures Apache Kafka broker"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          IO.read(File.join(File.dirname(__FILE__), "VERSION")) rescue "0.0.1"
+version          "1.0.1"
 
 %w{ ubuntu debian }.each do |os|
   supports os
