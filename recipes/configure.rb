8234bc21ff872e02d5306d5cf0ac57581bcc94a0
Daniel Somerfield <daniel@redwinewithfish.org>
2015-11-10 19:14:04 -0800

    Refactoring configuration code in preparation for supporting multiple brokers.

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -2,6 +2,9 @@
 # Cookbook Name:: apache_kafka
 # Recipe:: configure
 #
+# node["apache_kafka"]["brokers"].each do |broker|
+#   p "Brokers: #{broker.inspect}"
+# end
 
 [
   node["apache_kafka"]["config_dir"],
@@ -14,6 +17,8 @@
     owner node["apache_kafka"]["user"]
   end
 end
+
+
 
 %w{ kafka-server-start.sh kafka-run-class.sh kafka-topics.sh }.each do |bin|
   template ::File.join(node["apache_kafka"]["bin_dir"], bin) do
@@ -29,27 +34,38 @@ end
   end
 end
 
+def create_broker_configuration(broker_config)
+  zookeeper_connect = node["apache_kafka"]["zookeeper.connect"]
+  zookeeper_connect = "localhost:2181" if zookeeper_connect.nil?
+
+  template ::File.join(node["apache_kafka"]["config_dir"], broker_config["broker_config_file"]) do
+    source "properties/server.properties.erb"
+    owner "kafka"
+    action :create
+    mode "0644"
+    variables(
+      :broker_id => broker_config["broker_id"],
+      :port => broker_config["port"],
+      :zookeeper_connect => zookeeper_connect,
+      :log_dirs => broker_config["data_dir"],
+      :entries => broker_config["entries"]
+    )
+    notifies :restart, "service[kafka]", :delayed
+  end
+end
+
 broker_id = node["apache_kafka"]["broker.id"]
 broker_id = 0 if broker_id.nil?
 
-zookeeper_connect = node["apache_kafka"]["zookeeper.connect"]
-zookeeper_connect = "localhost:2181" if zookeeper_connect.nil?
+broker_config = {
+  "broker_id" => broker_id,
+  "port" => node["apache_kafka"]["port"],
+  "broker_config_file" => node["apache_kafka"]["conf"]["server"]["file"],
+  "data_dir" => node["apache_kafka"]["data_dir"],
+  "entries" => node["apache_kafka"]["conf"]["server"]["entries"]
+}
 
-template ::File.join(node["apache_kafka"]["config_dir"],
-                     node["apache_kafka"]["conf"]["server"]["file"]) do
-  source "properties/server.properties.erb"
-  owner "kafka"
-  action :create
-  mode "0644"
-  variables(
-    :broker_id => broker_id,
-    :port => node["apache_kafka"]["port"],
-    :zookeeper_connect => zookeeper_connect,
-    :log_dirs => node["apache_kafka"]["data_dir"],
-    :entries => node["apache_kafka"]["conf"]["server"]["entries"]
-  )
-  notifies :restart, "service[kafka]", :delayed
-end
+create_broker_configuration(broker_config)
 
 template ::File.join(node["apache_kafka"]["config_dir"],
                      node["apache_kafka"]["conf"]["log4j"]["file"]) do
---
+++ test/integration/multiple-instance/serverspec/multiple-instance_spec.rb
@@ -0,0 +1,62 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe "Kafka" do
+
+  describe "broker 0" do
+    it "is listening" do
+      expect(port(9092)).to be_listening
+    end
+
+    it "has JMX listening" do
+      expect(port(9192)).to be_listening
+    end
+
+    it "has a running service of kafka" do
+      expect(service("kafka-broker-0")).to be_running
+    end
+
+    it "has a log directory " do
+      expect(file("/var/log/kafka/broker-0")).to be_directory
+    end
+  end
+
+  describe "broker 1" do
+    it "is listening" do
+      expect(port(9093)).to be_listening
+    end
+
+    it "has JMX listening" do
+      expect(port(9193)).to be_listening
+    end
+
+    it "has a running service of kafka" do
+      expect(service("kafka-broker-1")).to be_running
+    end
+
+    it "has a log directory " do
+      expect(file("/var/log/kafka/broker-1")).to be_directory
+    end
+  end
+
+  describe "broker 2" do
+    it "is listening" do
+      expect(port(9094)).to be_listening
+    end
+
+    it "has JMX listening" do
+      expect(port(9194)).to be_listening
+    end
+
+    it "has a running service of kafka" do
+      expect(service("kafka-broker-2")).to be_running
+    end
+
+    it "has a log directory " do
+      expect(file("/var/log/kafka/broker-2")).to be_directory
+    end
+  end
+
+end
